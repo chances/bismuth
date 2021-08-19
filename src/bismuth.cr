e8ed79d8e393cab699015fea9d6de8ff8629c88b
Chance Snow <git@chancesnow.me>
2021-08-19 10:56:11 -0500

    Add Window abstraction, Integrate da1nerd/render_loop abstraction

--- src/bismuth.cr
+++ src/bismuth.cr
@@ -1,40 +1,40 @@
 require "glfw"
+require "render_loop"
 require "wgpu"
 
-require "./platform.cr"
+require "./window.cr"
 
-abstract class App
-  # Whether this App is in debug mode.
+# A Bismuth application.
+abstract class App < RenderLoop::Engine
+  # Whether this application is in debug mode.
   @debug = true
+  @frame_time : Float64
+  @active = false
+  @windows = Array(Window).new
+  # Graphics resources
   @adapter : WGPU::Adapter
-  @device : WGPU::Device
-  @windows = Array(Glfw::Window*).new
+  getter device : WGPU::Device
 
-  def initialize(name : String, width : UInt16 = 800, height : UInt16 = 600, fullscreen = false)
+  def initialize(name : String, width : UInt16 = 800, height : UInt16 = 600, fullscreen = false, desired_frame_rate = 60_f64)
     @name = name
-    @active = true
     {% if flag?(:release) %}
-      @debug = false
+    @debug = false
     {% end %}
+    @frame_time = 1.0f64 / desired_frame_rate
+
+    at_exit { puts "#{@name} app exited" }
+
     WGPU.set_log_level(@debug ? WGPU::LogLevel::Trace : WGPU::LogLevel::Warning)
 
     Glfw.set_error_callback ->(num, msg) { puts String.new(msg) }
     abort("Could not initialize GLFW", 1) unless Glfw.init
 
-    @primary_monitor = Glfw.get_primary_monitor
-
-    # Glfw.window_hint(Glfw::VISIBLE, false)
-    Glfw.window_hint(Glfw::CLIENT_API, Glfw::NO_API) # Graphics are handled by wgpu
-    @main_window = Glfw.create_window(width, height, name, fullscreen ? @primary_monitor : Pointer(Void).null, nil)
-    abort("Failed to initialize a new GLFW Window", 1) if @main_window.null?
+    @main_window = Window.new @name
     puts "Created main window"
+    @main_window.startup
     @windows.push @main_window
 
-    @surface = Platform.create_surface @name, @main_window
-    abort("Failed to create graphics surface", 1) unless @surface.is_valid?
-    puts "Created native graphics surface"
-
-    @adapter = WGPU::Adapter.request(@surface).get
+    @adapter = WGPU::Adapter.request(@main_window.surface).get
     abort("Failed to initialize graphics adapter") unless @adapter.is_ready?
     @device = WGPU::Device.request(
       @adapter,
@@ -44,15 +44,83 @@ abstract class App
     abort("Failed to initialize graphics device") unless @device.is_valid?
   end
 
+  # Called when the main loop is starting up.
+  # Use this to set things up.
+  protected def startup
+  end
+
   def run
-    Glfw.show_window @main_window
-    Glfw.poll_events
+    startup_time = Time.monotonic.total_milliseconds # When the loop started
+
+    @main_window.show
     puts "Main window shown"
 
+    self.startup
+
+    @active = true
+    last_time = Time.monotonic.total_milliseconds
+    unprocessed_time = 0_f64
+
     while @active
-      Glfw.poll_events
-      @active = false if Glfw.window_should_close(@main_window) == Glfw::TRUE
-      Fiber.yield
+      should_render = false
+      start_time = Time.monotonic.total_milliseconds
+      passed_time = start_time - last_time # How long the previous frame took
+      last_time = start_time
+      unprocessed_time += passed_time
+
+      while unprocessed_time > @frame_time
+        should_render = true
+        unprocessed_time -= @frame_time
+
+        Glfw.poll_events
+        @active = false if @main_window.should_close?
+        break unless @active
+
+        tick = RenderLoop::Tick.new(@frame_time, passed_time, startup_time)
+        self.tick tick, @main_window.input
+      end
+
+      # Sleep for 1 millisecond
+      sleep(Time::Span.new(nanoseconds: 1000000)) unless should_render
+
+      if should_render
+        self.render
+        self.flush
+
+        @windows.each do |window|
+          window.render
+          window.title= "#{@name} - Frame time: #{sprintf "%1.2d", passed_time}ms" if @debug
+        end
+      end
     end
+
+    self.shutdown
+
+    Glfw.terminate
+    puts "Main window destroyed"
+  end
+
+  # Called at each iteration of the main loop.
+  # This is when application state should be updated.
+  private def tick(tick : RenderLoop::Tick, input : RenderLoop::Input)
+    self.tick tick
+  end
+
+  # Called at each iteration of the main loop.
+  # This is when application state should be updated.
+  protected def tick(tick : RenderLoop::Tick)
+  end
+
+  # Called at intervals desigated by the configured frame rate.
+  # This is used to render the scene.
+  protected def render
+  end
+
+  # Called to perform cleanup operations after the sceen has been rendered.
+  protected def flush
+  end
+
+  # Called when the application is shutting down.
+  protected def shutdown
   end
 end
---
+++ src/window.cr
@@ -0,0 +1,105 @@
+require "glfw"
+require "render_loop"
+
+require "./input.cr"
+require "./platform.cr"
+
+alias Input = RenderLoop::Input(Key, MouseButton)
+
+class Window < RenderLoop::Window(Key, MouseButton)
+  @handle : Glfw::Window* = Pointer(Void).null
+  getter title : String
+  getter width : UInt16
+  getter height : UInt16
+  # Graphics resources
+  getter surface : WGPU::Surface?
+
+  def initialize(@title : String, @width : UInt16 = 800, @height : UInt16 = 600, @fullscreen = false, @cursor_visible = false)
+    @input = Input.new self
+    @primary_monitor = Glfw.get_primary_monitor
+
+    Glfw.window_hint(Glfw::VISIBLE, @visible = false)
+    Glfw.window_hint(Glfw::CLIENT_API, Glfw::NO_API) # Graphics are handled by wgpu
+    @handle = Glfw.create_window(@width, height, @title, @fullscreen ? Glfw.get_primary_monitor : Pointer(Void).null, nil)
+    abort("Failed to initialize a new GLFW Window", 1) if @handle.null?
+  end
+
+  def startup
+    @surface = Platform.create_surface @title, @handle
+    abort("Failed to create graphics surface", 1) unless @surface.as(WGPU::Surface).is_valid?
+    puts "Created native graphics surface"
+  end
+
+  def size : RenderLoop::Size
+    Glfw.get_window_size @handle, out width, out height
+    {@width = width, @height = height}
+  end
+
+  def size(s : RenderLoop::Size)
+    Glfw.set_window_size @handle, @width = s[0], @height = s[1]
+  end
+
+  def fullscreen?
+    @fullscreen
+  end
+
+  def visible?
+    @visible
+  end
+
+  def should_close? : Bool
+    Glfw.window_should_close(@handle) == Glfw::TRUE
+  end
+
+  def render
+  end
+
+  def destroy
+    Glfw.destroy_window @handle
+  end
+
+  def title=(title : String)
+    Glfw.set_window_title @handle, @title = title
+  end
+
+  def input
+    raise "Window input is nil" if @input.nil?
+    @input.as(Input)
+  end
+
+  def key_pressed?(k : Key) : Bool
+    Glfw.get_key(@handle, k.value) == Glfw.PRESS
+  end
+
+  def mouse_button_pressed?(b : MouseButton) : Bool
+    Glfw.get_mouse_button(@handle, b.value) == Glfw.PRESS
+  end
+
+  def cursor_position : RenderLoop::Position
+    Glfw.get_cursor_pos @handle, out x, out y
+    {x, y}
+  end
+
+  def cursor_position=(position : RenderLoop::Position)
+    Glfw.set_cursor_pos @handle, position[0], position[1]
+  end
+
+  def cursor_visible?
+    @cursor_visible
+  end
+
+  def cursor_visible=(visible : Bool)
+    Glfw.set_input_mode @handle, Glfw::CURSOR, visible ? Glfw::CURSOR_NORMAL : Glfw::CURSOR_DISABLED
+    @cursor_visible = get_input_mode(@handle, Glfw::CURSOR) == Glfw::CURSOR_NORMAL
+  end
+
+  def show
+    Glfw.show_window @handle
+    @visible = true
+  end
+
+  def hide
+    Glfw.hide_window @handle
+    @visible = false
+  end
+end
